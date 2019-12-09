c165768887c5e669b0a4f8053097a950a129e8d4
Yuliia Engman <yuliia.engman@gmail.com>
2019-12-08 20:01:19 -0500

    set layouts for all VC

---
+++ Concurrency-Lab/Concurrency-Lab/CountryTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CountryTableViewCell.swift
+//  Concurrency-Lab
+//
+//  Created by Yuliia Engman on 12/8/19.
+//  Copyright © 2019 Yuliia Engman. All rights reserved.
+//
+
+import UIKit
+
+class CountryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Concurrency-Lab/Concurrency-Lab/DetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailViewController.swift
+//  Concurrency-Lab
+//
+//  Created by Yuliia Engman on 12/8/19.
+//  Copyright © 2019 Yuliia Engman. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
